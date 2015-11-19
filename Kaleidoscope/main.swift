e06344d8b46a8abebb66f8b36ddbc613ca87cea9
Ben Cochran <bcochran@gmail.com>
2015-11-19 01:47:16 -0800

    Run analyzer on a test set of expressions

--- Kaleidoscope/main.swift
+++ Kaleidoscope/main.swift
@@ -8,13 +8,29 @@
 
 import Foundation
 import KaleidoscopeLang
-import LLVM
+import Either
 
-let ast = KaleidoscopeLang.parse("def add(a b) a + b;")
-print(ast)
 
-func llvmError(reason: UnsafePointer<Int8>) {
-    // Breakpoint-able error handler
-    fatalError(String.fromCString(reason)!)
+let lines = [
+    "extern addThree(a b c);",
+    "def add(a b) a + b;",
+    "def addThree(x y z) add(add(x y) z);"
+]
+
+let result = lines
+    // Parse each line and map errors to the correct type
+    // This is a crazier way to do this: (requires a `flip :: (a -> b -> c) -> b -> a -> c`)
+    //    .map(parseTopLevelExpression >>> Either<KaleidoscopeLang.Error,TopLevelExpression>.mapLeft |> flip <| Error.parseError)
+    .map { parseTopLevelExpression($0).mapLeft(Error.parseError).map({ $0 as Expression }) }
+    // Compact the array of `Either`s into a single `Either` of `Array`
+    .compact()
+    // Analize the expressions
+    .flatMap(Analyzer.analyze)
+
+if case let .Left(error) = result {
+    print("Failed: \(error)")
+    exit(1)
+} else {
+    print("Success!")
+    exit(0)
 }
-LLVMInstallFatalErrorHandler(llvmError)
