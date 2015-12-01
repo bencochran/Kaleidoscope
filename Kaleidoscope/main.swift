042cb4e1dbfe4e41cc4a2d175783d68c257b4470
Ben Cochran <bcochran@gmail.com>
2015-12-01 16:57:54 -0600

    Decompose main.swift a little

--- Kaleidoscope/main.swift
+++ Kaleidoscope/main.swift
@@ -11,10 +11,52 @@ import LLVM
 import Either
 import Prelude
 
+func compileModule(lines: [String]) -> Either<Error, CodegenContext> {
+    let context = CodegenContext(moduleName: "kaleidoscope", context: Context.globalContext)
+
+    return compile(lines, inContext: context) >>- const(.right(context))
+}
+
+private func compile(lines: [String], inContext context: CodegenContext) -> Either<Error, [ValueType]> {
+    return parseLines(lines) >>- Analyzer.analyze >>- codegen(context)
+}
+
+private func parseLines(lines: [String]) -> Either<Error, [Expression]> {
+    return lines
+        .map {
+            // Parse each expression
+            return parseTopLevelExpression($0)
+                // Wrap errors
+                .mapLeft(Error.parseError)
+                // Cast as Expression
+                .map(id)
+                // Lift `main` to a MainExpression
+                .flatMap(liftMain)
+        }
+        // Compact the array of `Either`s into a single `Either` of `Array`
+        .compact()
+}
+
+private func codegen(context: CodegenContext)(expressions: [Expression]) -> Either<Error, [ValueType]> {
+    return expressions
+        // Attempt to cast each Expression to Codegenable
+        .map(attemptCast)
+        // Compact the array of `Either`s into a single `Either` of `Array`
+        .compact()
+        // Perform code generation in the context
+        .flatMapEach(codegenInContext(context))
+}
+
+func extractModule(context: CodegenContext) -> Either<Error, String> {
+    guard let ir = context.module.string else {
+        return .left(Error(kind: .CodegenError, message: "Unable to generate IR"))
+    }
+    return .right(ir)
+}
+
 var stdout = OutputStream.Out
 var stderr = OutputStream.Err
 
-let context = CodegenContext(moduleName: "kaleidoscope", context: Context.globalContext)
 
 let lines = [
     "extern addThree(a b c);",
@@ -23,34 +65,7 @@ let lines = [
     "def addThree(x y z) add(add(x y) z);"
 ]
 
-let result = lines
-    .map {
-        // Parse each expression
-        return parseTopLevelExpression($0)
-            // Wrap errors
-            .mapLeft(Error.parseError)
-            // Cast as Expression
-            .map(id)
-            // Lift `main` to a MainExpression
-            .flatMap(liftMain)
-    }
-    // Compact the array of `Either`s into a single `Either` of `Array`
-    .compact()
-    // Analize the expressions
-    .flatMap(Analyzer.analyze)
-    // Attempt to cast each Expression to Codegenable
-    .flatMapEach(attemptCast)
-    // Perform code generation in the context
-    .flatMapEach(codegenInContext(context))
-
-if case let .Left(error) = result {
-    print("Failed: \(error)", toStream: &stderr)
-    exit(1)
-}
-
-guard let ir = context.module.string else {
-    print("Failed to generate IR", toStream: &stderr)
-    exit(1)
-}
-
-print(ir, toStream: &stdout)
+compileModule(lines)
+    .flatMap(extractModule)
+    .mapLeft { String("Failed: \($0)") }
+    .materialize(leftStream: &stderr, rightStream: &stdout)
